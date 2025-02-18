b27b95b654bc32cd2cc35066ae18afe512af7910
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-18 23:02:48 +0600

    update layout

---
+++ nextmart-client/src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  );
+};
+
+export default HomePage;
