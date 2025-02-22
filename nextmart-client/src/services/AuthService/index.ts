d2ab012b070a804bd88c10d37ec7b3b9f5c8eb96
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-23 02:41:53 +0600

    update user data

--- nextmart-client/src/services/AuthService/index.ts
+++ nextmart-client/src/services/AuthService/index.ts
@@ -68,7 +68,7 @@ export const reCaptchaTokenVerification = async (token: string) => {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams({
-        secret: process.env.NEXT_PUBLIC_RECAPTCHA_SERVER_KEY!,
+        secret: process.env.NEXT_PUBLIC_RECAPTCHA_SERVER_KEYS!,
         response: token,
       }),
     });
