c3663ae346656ece8c093dfcc16bf7a73764973e
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-25 00:35:36 +0600

    hero section add

---
+++ nextmart-client/src/app/(withCommonLayout)/(home)/page.tsx
@@ -0,0 +1,17 @@
+import Category from "@/components/modules/home/Category";
+import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
+import HeroSection from "@/components/modules/home/HeroSection";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Category />
+      <FeaturedProducts />
+      {/* <FlashSale /> */}
+      {/* <TopBrands /> */}
+    </div>
+  );
+};
+
+export default HomePage;
