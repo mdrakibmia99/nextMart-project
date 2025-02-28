f2b224c4155689600492e88a5fdb29b55c3abbdd
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-03-01 00:44:27 +0600

    update backend again,solve error

--- nextmart-client/src/app/(withCommonLayout)/cart/page.tsx
+++ nextmart-client/src/app/(withCommonLayout)/cart/page.tsx
@@ -1,3 +1,5 @@
+
+import Address from "@/components/modules/cart/Address";
 import CartProducts from "@/components/modules/cart/CartProducts";
 import Coupon from "@/components/modules/cart/Coupon";
 import ProductBanner from "@/components/modules/products/banner";
@@ -8,8 +10,10 @@ const CartPage = () => {
     <NMContainer>
       <ProductBanner title="Cart Page" path="Home - Cart" />
       <div className="grid grid-cols-12 gap-8 my-5">
-        <CartProducts/>
+        <CartProducts />
         <Coupon />
+        <Address />
+        {/* <PaymentDetails /> */}
       </div>
     </NMContainer>
   );
